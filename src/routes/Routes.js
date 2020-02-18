295797acac7f5b5588df888fe7b6095e0abab0db
Renan Henrique <henriquebasshvf@gmail.com>
2020-02-18 10:41:31 -0300

    update

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -3,11 +3,14 @@ import * as views from '../views'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
 
-const AppNavigator = createStackNavigator({
-  Home: { screen: views.Home },
-  // Insert views here
-},{
-  headerMode: "none"
-})
+const AppNavigator = createStackNavigator(
+  {
+    Home: { screen: views.Home },
+    // Insert views here
+  },
+  {
+    headerMode: 'none',
+  }
+)
 
 export default createAppContainer(AppNavigator)
