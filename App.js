164a0021d82b0809af28f6f9e1ffafe4674ba629
Renan Henrique <henriquebasshvf@gmail.com>
2020-06-13 19:12:32 -0300

    update

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import { setNavigator } from './src/utils/navigation'
 import Routes from './src/routes/Routes'
 import store from './src/redux/store'
+import './src/configs/ReactotronConfig'
 
 export default class App extends Component {
   render() {
